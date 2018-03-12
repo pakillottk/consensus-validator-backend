cd288ab79983b035865ad3c4b3a6c8473ca90aa4
Francisco Gázquez <pmgmsc@gmail.com>
2018-03-12 19:20:22 +0100

    ADDED Code query

--- Database/Code.js
+++ Database/Code.js
@@ -1,9 +1,23 @@
 const Model = require( './Model' );
+const Type = require( './Type' );
 
 class Code extends Model {
     static get tableName() {
         return 'Codes';
     }
+
+    static get relationMappings() {
+        return {
+            type: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Type,
+                join: {
+                    from:'Codes.type_id',
+                    to: 'Types.id'
+                }
+            },
+        }
+    };
 }
 
 module.exports = Code;
--- Routes/Code.js
+++ Routes/Code.js
@@ -1,2 +1,19 @@
+const Type = require( '../Database/Type' );
 const CodeModel = require( '../Database/Code' );
-module.exports = require( './ModelRouter' )( CodeModel );
+
+module.exports = require( './ModelRouter' )( CodeModel, '[type]', async ( req ) => {
+    const sessionId = req.query.session;
+    if( !sessionId ) {
+        return new DBQuery( req );
+    }
+
+    const sessionTypes = await Type.query().select('id').where( 'session_id', sessionId );
+    const typesArray = [];
+    sessionTypes.forEach( type => {
+        typesArray.push( type.id );
+    });
+    const dbQuery = new DBQuery( req );
+    dbQuery.addClause( 'type_id', 'in', typesArray );
+
+    return dbQuery;
+});
