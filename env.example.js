dc0ea38ff4de74ae8d4863d04b62188a5240c49d
Francisco Gázquez <pmgmsc@gmail.com>
2018-05-15 12:49:38 +0200

    UPDATED env format

--- env.example.js
+++ env.example.js
@@ -1,3 +1,10 @@
 module.exports = {
-    PROJECT_DIR: '' //The root dir of the server
+    PROJECT_DIR: '', //The root dir of the server
+    protocol: 'http', //Serve on http or https
+    serverOptions: { //Server config
+        ssl: {      
+            keyFile: '', //path to the ssl_key
+            certFile: '' //path to the ssl_cert
+        }
+    }
 }
