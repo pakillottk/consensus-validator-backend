3aaa0e1c3490db98b3647e8df0e56c8116470896
Francisco Gázquez <pmgmsc@gmail.com>
2018-06-19 13:00:17 +0200

    ADDED Sales  by zones

--- Controllers/SaleController.js
+++ Controllers/SaleController.js
@@ -1,6 +1,7 @@
 const ModelController = require('./ModelController').class
 const Session = require('../Database/Session');
 const Deliver = require( '../Database/Deliver' );
+const SeatReserve = require( '../Database/SeatReserve' );
 const Type = require( '../Database/Type' );
 const Code = require( '../Database/Code' );
 const LogEntry = require('../Database/LogEntry');
@@ -15,7 +16,15 @@ class SaleController extends ModelController {
         this.jobsResponses = {};
         this.queue = kue.createQueue();
         this.queue.process( 'sale', ( job, done ) => {
-            this.createSale( job.id, job.data.data, job.data.including, job.data.query, job.data.user, done );
+            this.createSale( 
+                job.id, 
+                job.data.data, 
+                job.data.including, 
+                job.data.query, 
+                job.data.session, 
+                job.data.user, 
+                done 
+            );
         });
     }
 
@@ -59,7 +68,7 @@ class SaleController extends ModelController {
         return soldByMe;
     }
 
-    async createSale( id, data, including, query, user, done ) { 
+    async createSaleByTypes( id, data, including, query, user, done ) { 
         const res = this.jobsResponses[ id ];
         if( !res ) {
             throw new Error( "There's no response object for this job: " + job );
@@ -149,6 +158,93 @@ class SaleController extends ModelController {
         delete this.jobsResponses[ id ];
     }
 
+    async createSaleByZones( id, data, including, query, session, user, done ) {
+        const res = this.jobsResponses[ id ];
+        if( !res ) {
+            throw new Error( "There's no response object for this job: " + job );
+        }       
+
+        //Reserved, attempt to make the sale
+        let trx;
+        try {
+            //Check if exists a reserve for user and seat
+            const reserve = await SeatReserve.query()
+            .where('session_id', '=', session.id)
+            .andWhere( 'zone_id', '=', data.zone_id )
+            .andWhere( 'seat_row', '=', data.row_index )
+            .andWhere( 'seat_index', '=', data.seat_index )
+            .andWhere( 'user_id', '=', user.id )
+            .andWhere( 'expires_at', '>', new Date() );
+            //if no reserve, cancel the selling process
+            if( reserve.length === 0 ) {
+                res.status( 400 ).send( {error:{message:'Seat not reserved or no by this seller'}} );
+                done( new Error( 'Seat not reserved or no by this seller' ) );
+            }
+
+            trx = await transaction.start( this.model.knex() );
+            const hashData = data.user_id + "" + data.type_id + "" + new Date().toString() + "" + new Date().getTime(); 
+            const hashCode = crypto.createHash('md5').update(hashData).digest("hex");
+            const newCode = await Code.query( trx ).insert({
+                code: 'CNS' + hashCode.substr(0, 9),
+                name: data.name,
+                type_id: data.type_id,
+                email: data.email,
+                validations: 0,
+                maxValidations: 1,
+                out: true,
+                zone_id: data.zone_id,
+                row_index: data.row_index,
+                seat_index: data.seat_index,
+                seat_number: data.seat_number,
+                created_at: new Date(),
+                updated_at: new Date()
+            });
+            const sale = await this.model.query( trx ).eager( including ).insert({
+                user_id: user.id,
+                code_id: newCode.id,
+                created_at: new Date(),
+                updated_at: new Date()
+            });
+            //make reserve never expire, locking the seat
+            await SeatReserve.query( trx ).patchAndFetchById( reserve[0].id, {
+                user_id: null,
+                expires_at: null
+            });
+
+            await trx.commit();
+            
+            res.status( 200 ).send( sale );
+            done();
+        } catch( error ) {
+            if( trx ) {
+                await trx.rollback();
+            }
+
+            res.status( 400 ).send( error );
+            done( new Error( error ) );
+        }
+    }
+
+    async createSale( id, data, including, query, session, user, done ) {
+        const res = this.jobsResponses[ id ];
+        switch( session.ticketing_flow ) {
+            case 'by_types': {
+                await this.createSaleByTypes( id, data, including, query, user, done  );
+                break
+            }
+            case 'by_zones':{
+                await this.createSaleByZones( id, data, including, query, session, user, done );
+                break
+            }
+            default:{
+                res.status( 400 ).send( {error:{message:'Uknown ticketing flow in Session'}} );
+                done( new Error( 'Unknown ticketing_flow' ) );
+            }
+        }
+
+        delete this.jobsResponses[ id ];
+    }
+
     async create( data, including, query, res ) {
         const user = res.locals.oauth.token.user;        
         
@@ -192,8 +288,8 @@ class SaleController extends ModelController {
             }
         }
 
-        //If tikcetoffice is open for the user, attempt to sell
-        const jobData =  { data, including: including, query: query, user: {id: user.id, username: user.username} };
+        //If ticketoffice is open for the user, attempt to sell
+        const jobData =  { data, including: including, query: query, session: session[0], user: {id: user.id, username: user.username} };
         const job = this.queue.create( 'sale', jobData ).save(
             ( error ) => {  
                 if( !error ){
